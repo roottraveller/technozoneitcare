31f3d606848e616220d949abd429d5b8e909f12a
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-03-08 17:38:43 +0530

    form recaptch

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -9,28 +9,30 @@ export default function Subscribe() {
                         <i className="far fa-paper-plane fa-2x mb-2 text-white"/>
                         <h2 className="text-white mb-5">Subscribe to receive updates!</h2>
 
-                            <form name="subscribe" method="POST"
-                                  data-netlify="true"
-                                  data-netlify-honeypot="bot-field"
-                                  data-netlify-recaptcha='true'
-                                  className="form-inline d-flex">
-                                <input type="hidden" name="form-name" value="subscribe" />
-                                <input type="hidden" name="bot-field" />
+                        <form name="subscribe"
+                              method="POST"
+                              action="POST"
+                              data-netlify="true"
+                              data-netlify-honeypot="bot-field"
+                              className="form-inline d-flex">
+                            <input type="hidden" name="form-name" value="subscribe"/>
+                            <input type="hidden" name="bot-field"/>
 
-                                <input
-                                    type="email"
-                                    id="inputEmail"
-                                    name="subscriber-email"
-                                    placeholder="Enter email address..."
-                                    className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
-                                />
-                                <button type="submit" className="btn btn-primary mx-auto">
-                                    Subscribe
-                                </button>
-                            </form>
-                        </div>
+                            <input
+                                type="email"
+                                id="inputEmail"
+                                name="subscriber-email"
+                                placeholder="Enter email address..."
+                                className="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0"
+                            />
+                            <span data-netlify-recaptcha='true'/>
+                            <button type="submit" className="btn btn-primary mx-auto">
+                                Subscribe
+                            </button>
+                        </form>
                     </div>
                 </div>
-            </section>
-        );
-    }
+            </div>
+        </section>
+    );
+}
