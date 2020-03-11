52d35b782bba9ef8a31d4b19a9d3acee17e1b3a0
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-03-11 15:18:53 +0530

    serviecs sections updaed

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import demoImage1 from "../assets/images/demo-image-01.jpg";
-import demoImage2 from "../assets/images/demo-image-02.jpg";
+import demoImage1 from "../assets/images/phone.jpeg";
+import demoImage2 from "../assets/images/phone1.jpg";
 
 export default function Services() {
     return (
@@ -19,10 +19,9 @@ export default function Services() {
                         <div className="bg-black text-center h-100 project">
                             <div className="d-flex h-100">
                                 <div className="project-text w-100 my-auto text-center text-lg-left">
-                                    <h4 className="text-white">Misty</h4>
+                                    <h4 className="text-white">CCTV Camera & Spy Products</h4>
                                     <p className="mb-0 text-white-50">
-                                        An example of where you can put an image of a project, or
-                                        anything else, along with a description.
+                                        An example of text here
                                     </p>
                                     <hr className="d-none d-lg-block mb-0 ml-0"/>
                                 </div>
@@ -39,13 +38,148 @@ export default function Services() {
                         <div className="bg-black text-center h-100 project">
                             <div className="d-flex h-100">
                                 <div className="project-text w-100 my-auto text-center text-lg-right">
-                                    <h4 className="text-white">Mountains</h4>
+                                    <h4 className="text-white">Smart Time Attendance & Access Control</h4>
                                     <p className="mb-0 text-white-50">
-                                        Another example of a project with its respective
-                                        description. These sections work well responsively as well,
-                                        try this theme on a small screen!
+                                        An example of text here
                                     </p>
                                     <hr className="d-none d-lg-block mb-0 mr-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage1} alt=""/>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 className="text-white">Smart Door Access & Alerm System</h4>
+                                    <p className="mb-0 text-white-50">
+                                        An example of text here
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 ml-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage2} alt=""/>
+                    </div>
+                    <div className="col-lg-6 order-lg-first">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 className="text-white">Biometric Card Reader</h4>
+                                    <p className="mb-0 text-white-50">
+                                        An example of text here
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 mr-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage1} alt=""/>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 className="text-white">Finger Print lock & Door Interlocking System</h4>
+                                    <p className="mb-0 text-white-50">
+                                        An example of text here
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 ml-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage2} alt=""/>
+                    </div>
+                    <div className="col-lg-6 order-lg-first">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 className="text-white">Fire Alarm System</h4>
+                                    <p className="mb-0 text-white-50">
+                                        An example of text here
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 mr-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage1} alt=""/>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 className="text-white">Server & Data Center Solutions</h4>
+                                    <p className="mb-0 text-white-50">
+                                        An example of text here
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 ml-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage2} alt=""/>
+                    </div>
+                    <div className="col-lg-6 order-lg-first">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 className="text-white">Home & Office Electric Wiring
+                                    </h4>
+                                    <p className="mb-0 text-white-50">
+                                        An example of text here
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 mr-0"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center no-gutters mb-5 mb-lg-0">
+                    <div className="col-lg-6">
+                        <img className="img-fluid" src={demoImage1} alt=""/>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="bg-black text-center h-100 project">
+                            <div className="d-flex h-100">
+                                <div className="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 className="text-white">Household Scrap</h4>
+                                    <p className="mb-0 text-white-50">
+                                        Electronics appliances,
+                                        Electronic Scrap,
+                                        Electrical Scrap,
+                                        Battery Recycling
+                                    </p>
+                                    <hr className="d-none d-lg-block mb-0 ml-0"/>
                                 </div>
                             </div>
                         </div>
