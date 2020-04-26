0ee14d3fc0d155b49317801b1ef4aad9f13baec0
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-04-26 13:09:03 +0530

    added portfolid bug fix

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,6 @@
-import React from "react";
-import config from "../../config";
+import React from 'react';
+import config from '../../config';
+import solo2 from '../assets/images/portfolio/solo2.jpg';
 
 export default function Portfolio() {
     return (
@@ -8,23 +9,26 @@ export default function Portfolio() {
                 <h1 className="text-black mb-4 text-monospace text-center">Who has worked with us</h1>
                 <hr className="my-4"/>
 
-                <div className="portfolio-items">
+
+                <div className="row justify-content-center no-gutters">
                     {config.portfolioEntry.map((portfolio, idx) => {
-                        const {image, service, address} = portfolio;
+                        const { image, service, address } = portfolio;
                         return (
-                            <div className="box" key={idx}>
-                                <div className="imgBx">
-                                    <img className="img-fluid" src={image} alt=""/>
-                                </div>
-                                <div className="content">
-                                    <h3>{service}</h3>
-                                    <p>{address}</p>
+                            <div className="col-lg-4 col-md-6 portfolio-items" key={idx}>
+                                <div className="box">
+                                    <div className="imgBx">
+                                        <img className="img-fluid" src={solo2} alt=""/>
+                                        <p className="text-on-image">{address}</p>
+                                    </div>
+                                    <div className="content">
+                                        <h3>{service}</h3>
+                                        <p>{address}</p>
+                                    </div>
                                 </div>
                             </div>
                         );
                     })}
                 </div>
-
             </div>
         </section>
     );
