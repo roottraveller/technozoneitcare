47c68cf3e2f561bcfd36a35bcbb1fc3fe29cb557
Rishikesh Maurya <rishikesh.maurya@myntra.com>
2020-05-03 17:36:52 +0530

    added gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,42 @@
 const config = require('./config');
 
 module.exports = {
-  pathPrefix: config.pathPrefix,
-  siteMetadata: {
-    title: config.siteTitle,
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: config.manifestName,
-        short_name: config.manifestShortName,
-        start_url: config.pathPrefix || config.manifestStartUrl,
-        background_color: config.manifestBackgroundColor,
-        theme_color: config.manifestThemeColor,
-        display: config.manifestDisplay,
-        icon: config.manifestIcon, // This path is relative to the root of the site.
-      },
+    pathPrefix: config.pathPrefix,
+    siteMetadata: {
+        title: config.siteTitle,
     },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-transformer-json',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/assets/data`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: config.manifestName,
+                short_name: config.manifestShortName,
+                start_url: config.pathPrefix || config.manifestStartUrl,
+                background_color: config.manifestBackgroundColor,
+                theme_color: config.manifestThemeColor,
+                display: config.manifestDisplay,
+                icon: config.manifestIcon, // This path is relative to the root of the site.
+            },
+        },
+        'gatsby-plugin-sass',
+        'gatsby-plugin-offline',
+    ],
 };
